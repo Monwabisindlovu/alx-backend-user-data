0b73bea6333810e20d7e48529a890c269a03ba6d
monwabisi ndlovu <monwabisindlovu78@gmail.com>
2024-05-21 10:57:16 +0000

    app.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-""" Module of Index views """
+""" Module of Index views
+"""
 from flask import jsonify, abort
 from api.v1.views import app_views
+
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
@@ -10,6 +12,7 @@ def status() -> str:
       - the status of the API
     """
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
@@ -22,11 +25,20 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
     Return:
-      - a 401 error response
+      - raises a 401 error by using abort
     """
     abort(401)
 
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - raises a 403 error by using abort
+    """
+    abort(403)
