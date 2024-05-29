581f376d4019318c9ee52461c9f687ad7f1d3327
monwabisi ndlovu <monwabisindlovu78@gmail.com>
2024-05-29 08:05:33 +0000

    auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """
-Auth module for handling password hashing.
+Auth module for handling user authentication.
 """
 import bcrypt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -20,6 +22,50 @@ def _hash_password(password: str) -> bytes:
     return hashed_password
 
 
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Register a new user with email and password.
+
+        Args:
+            email (str): The email of the user.
+            password (str): The password of the user.
+
+        Returns:
+            User: The User instance created and added to the database.
+
+        Raises:
+            ValueError: If a user with the email already exists.
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
+
+
 # Example usage
 if __name__ == "__main__":
-    print(_hash_password("Hello Holberton"))
+    email = 'me@me.com'
+    password = 'mySecuredPwd'
+
+    auth = Auth()
+
+    try:
+        user = auth.register_user(email, password)
+        print("successfully created a new user!")
+    except ValueError as err:
+        print(f"could not create a new user: {err}")
+
+    try:
+        user = auth.register_user(email, password)
+        print("successfully created a new user!")
+    except ValueError as err:
+        print(f"could not create a new user: {err}")
